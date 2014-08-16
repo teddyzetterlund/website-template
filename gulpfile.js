cb2a1b77c175e6acb5aa5205b40d65af1610508b
Teddy Zetterlund <teddy.zetterlund@gmail.com>
2014-08-16 09:48:06 +0200

    Add styles task for basic styling support
    Closes #5

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp        = require('gulp');
 var spawn       = require('child_process').spawn;
 var browserSync = require('browser-sync');
+var prefix      = require('gulp-autoprefixer');
 
 gulp.task('jekyll-build', function(done) {
   spawn('jekyll', ['build'], {stdio: 'inherit'})
@@ -19,8 +20,16 @@ gulp.task('reload-server', ['jekyll-build'], function() {
   browserSync.reload();
 });
 
+gulp.task('styles', function() {
+  gulp.src('site.css')
+    .pipe(prefix())
+    .pipe(gulp.dest('_site/'))
+    .pipe(browserSync.reload({stream: true}));
+});
+
 gulp.task('watch', ['jekyll-build'], function() {
-  gulp.watch(['**/*.html', '!_site/**/*'], ['reload-server']);
+  gulp.watch(['**/*.html', '!_site/**/*'], ['reload-server', 'styles']);
+  gulp.watch('site.css', ['styles']);
 });
 
 
